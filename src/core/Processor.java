937e1916153a4a2f7c89a1fababf5b5b052f31f1
DodgerAkame <remy.simon@uha.fr>
2016-12-08 20:08:08 +0100

    Changed some methods' arguments

--- src/core/Processor.java
+++ src/core/Processor.java
@@ -28,11 +28,9 @@ import java.awt.image.RescaleOp;
 import java.awt.image.ShortLookupTable;
 import java.io.File;
 import java.io.IOException;
-<<<<<<< HEAD
-import javafx.scene.chart.BubbleChart;
-=======
->>>>>>> master
+
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 /**
  *
@@ -187,7 +185,7 @@ public class Processor {
      * @param channel should be RED, GREEN or BLUE
      * @throws IOException
      */
-    public void splitRGB(Graphics g, String path, String channel) throws IOException {
+    public ImageIcon splitRGB(String path, String channel) throws IOException {
         if (!is8bitgray || !isFrequency) {
             ImagePlus ip = new ImagePlus("image", ImageIO.read(new File(path)));
             ImagePlus[] channels = ChannelSplitter.split(ip);
@@ -200,8 +198,9 @@ public class Processor {
                 img = channels[2].getImage();
             }
 
-            g.drawImage(img, 0, 0, ip);
+            return new ImageIcon(img);
         }
+        return new ImageIcon(ImageIO.read(new File(path)));
     }
 
     /**
@@ -213,7 +212,7 @@ public class Processor {
      * @param matrix input type is like (1, 2, 3, 4, 5, 6, 7, 8, 9)
      * @throws IOException
      */
-    public void convolution3(Graphics g, String path, int[] matrix) throws IOException {
+    public ImageIcon convolution3(String path, int[] matrix) throws IOException {
         if (!is8bitgray && !isFrequency) {
             ImagePlus ip = new ImagePlus("image", ImageIO.read(new File(path)));
             ImageProcessor iproc = ip.getProcessor();
@@ -221,8 +220,9 @@ public class Processor {
             iproc.convolve3x3(matrix);
             Image img = iproc.getBufferedImage();
 
-            g.drawImage(img, 0, 0, ip);
+            return new ImageIcon(img);
         }
+        return new ImageIcon(ImageIO.read(new File(path)));
     }
 
     /**
@@ -235,62 +235,65 @@ public class Processor {
      * reaffected to 127
      * @throws IOException
      */
-    public void binarize(Graphics g, String path, int trigger) throws IOException {
+    public ImageIcon binarize(String path, int trigger) throws IOException {
         if (!is8bitgray || !isFrequency) {
             is8bitgray = true;
+
+            if (trigger > 255 || trigger < 0) {
+                trigger = 127;
+            }
+
+            ImagePlus ip = new ImagePlus("image", ImageIO.read(new File(path)));
+            ImageProcessor iproc = ip.getProcessor();
+            ImageProcessor grayproc = iproc.convertToByte(true);
+            grayproc.threshold(trigger);
+            Image binary = grayproc.getBufferedImage();
+
+            return new ImageIcon(binary);
         }
-        if (trigger > 255 || trigger < 0) {
-            trigger = 127;
-        }
-
-        ImagePlus ip = new ImagePlus("image", ImageIO.read(new File(path)));
-        ImageProcessor iproc = ip.getProcessor();
-        ImageProcessor grayproc = iproc.convertToByte(true);
-        grayproc.threshold(trigger);
-        Image binary = grayproc.getBufferedImage();
-
-        g.drawImage(binary, 0, 0, ip);
-
+        return new ImageIcon(ImageIO.read(new File(path)));
     }
 
-    public void doDFT(Graphics g, String path) throws IOException {
-
-        BufferedImage img = ImageIO.read(new File("src" + File.separatorChar + "image.jpg"));
-        int size = 2;
-        while (size < img.getHeight() && size < img.getWidth()) {
-            size = size * 2;
-        }
-        BufferedImage bi = new BufferedImage(size, size, BufferedImage.TYPE_BYTE_GRAY);
-        bi.getGraphics().drawImage(img, 0, 0, null);
-        int[] pixels = new int[bi.getHeight() * bi.getWidth()];
-        int counter = 0;
-        for (int i = 0; i < bi.getWidth(); i++) {
-            for (int j = 0; j < bi.getHeight(); j++) {
-                pixels[counter] = bi.getRGB(i, j);
-                counter++;
+    public ImageIcon doDFT(String path) throws IOException {
+        if (!isFrequency) {
+            BufferedImage img = ImageIO.read(new File("src" + File.separatorChar + "image.jpg"));
+            int size = 2;
+            while (size < img.getHeight() && size < img.getWidth()) {
+                size = size * 2;
             }
-        }
-
-        FFT fft = new FFT(pixels, bi.getWidth(), bi.getHeight());
-        int[] results = fft.getPixels();
-
-        BufferedImage fftimg = new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
-
-        counter = 0;
-        for (int i = 0; i < fftimg.getWidth(); i++) {
-            for (int j = 0; j < fftimg.getHeight(); j++) {
-                fftimg.setRGB(i, j, results[counter]);
-                counter++;
+            BufferedImage bi = new BufferedImage(size, size, BufferedImage.TYPE_BYTE_GRAY);
+            bi.getGraphics().drawImage(img, 0, 0, null);
+            int[] pixels = new int[bi.getHeight() * bi.getWidth()];
+            int counter = 0;
+            for (int i = 0; i < bi.getWidth(); i++) {
+                for (int j = 0; j < bi.getHeight(); j++) {
+                    pixels[counter] = bi.getRGB(i, j);
+                    counter++;
+                }
             }
-        }
 
-        g.drawImage(fftimg.getScaledInstance(512, 512, BufferedImage.SCALE_REPLICATE), 0, 0, null);
-        isFrequency = true;
+            FFT fft = new FFT(pixels, bi.getWidth(), bi.getHeight());
+            int[] results = fft.getPixels();
+
+            BufferedImage fftimg = new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+
+            counter = 0;
+            for (int i = 0; i < fftimg.getWidth(); i++) {
+                for (int j = 0; j < fftimg.getHeight(); j++) {
+                    fftimg.setRGB(i, j, results[counter]);
+                    counter++;
+                }
+            }
+
+            isFrequency = true;
+            return new ImageIcon(fftimg);
+        }
+        return new ImageIcon(ImageIO.read(new File(path)));
     }
 
     //TODO Problème ici
-    public void doIDFT(Graphics g, String path, int w, int h) throws IOException {
-        if (!isFrequency) {
+    public ImageIcon doIDFT(String path, int w, int h) throws IOException {
+        if (isFrequency) {
             BufferedImage bi = ImageIO.read(new File(path));
             int[] pixels = new int[bi.getHeight() * bi.getWidth()];
             int counter = 0;
@@ -317,12 +320,13 @@ public class Processor {
                 }
             }
 
-            g.drawImage(result, 0, 0, null);
             isFrequency = false;
+            return new ImageIcon(result);
         }
+        return new ImageIcon(ImageIO.read(new File(path)));
     }
 
-    public void contrast(Graphics g, String path, String how) throws IOException {
+    public ImageIcon contrast(String path, String how) throws IOException {
         if (!is8bitgray || !isFrequency) {
 
             BufferedImage img = ImageIO.read(new File(path));
@@ -335,11 +339,12 @@ public class Processor {
 
             RescaleOp op = new RescaleOp(1f, contrastFactor, null);
             img = op.filter(img, img);
-            g.drawImage(img, 0, 0, null);
+            return new ImageIcon(img);
         }
+        return new ImageIcon(ImageIO.read(new File(path)));
     }
 
-    public void brightness(Graphics g, String path, String how) throws IOException {
+    public ImageIcon brightness(String path, String how) throws IOException {
         if (!is8bitgray || !isFrequency) {
             BufferedImage img = ImageIO.read(new File(path));
             float scaleFactor = 0;
@@ -351,92 +356,76 @@ public class Processor {
 
             RescaleOp op = new RescaleOp(scaleFactor, 0, null);
             img = op.filter(img, img);
-            g.drawImage(img, 0, 0, null);
+            return new ImageIcon(img);
         }
-
+        return new ImageIcon(ImageIO.read(new File(path)));
     }
 
-<<<<<<< HEAD
-    public void posterize(Graphics g, String path, int level) throws IOException {
+    public ImageIcon posterize(String path, int level) throws IOException {
+        if (!is8bitgray || !isFrequency) {
+            BufferedImage bi = ImageIO.read(new File(path));
 
-        BufferedImage bi = ImageIO.read(new File(path));
-
-        int[] pixels = new int[bi.getHeight() * bi.getWidth()];
-        int counter = 0;
-        for (int i = 0; i < bi.getWidth(); i++) {
-            for (int j = 0; j < bi.getHeight(); j++) {
-                pixels[counter] = bi.getRGB(i, j);
-                counter++;
-            }
-        }
-
-        int[] levels = new int[level + 1];
-        int reste = 255 % level;
-        for (int i = 0; i < level; i++) {
-            levels[i] = (i + 1) * ((255 - reste) / level);
-        }
-        levels[level] = 255;
-
-        for (int i = 0; i < pixels.length; i++) {
-            int pixel = pixels[i];
-            int red = (pixel & 0x00ff0000) >> 16;
-            int green = (pixel & 0x0000ff00) >> 8;
-            int blue = (pixel & 0x000000ff);
-
-            boolean redChanged = false;
-            boolean greenChanged = false;
-            boolean blueChanged = false;
-            for (int j = 0; j < level; j++) {
-                if (red < levels[j] && !redChanged) {
-                    red = (int) Math.floor(levels[j] / 2);
-                    redChanged = true;
-                }
-                if (green < levels[j] && !greenChanged) {
-                    green = (int) Math.floor(levels[j] / 2);
-                    greenChanged = true;
-                }
-                if (blue < levels[j] && !blueChanged) {
-                    blue = (int) Math.floor(levels[j] / 2);
-                    blueChanged = true;
-                }
-
-                if (redChanged && greenChanged && blueChanged) {
-                    break;
+            int[] pixels = new int[bi.getHeight() * bi.getWidth()];
+            int counter = 0;
+            for (int i = 0; i < bi.getWidth(); i++) {
+                for (int j = 0; j < bi.getHeight(); j++) {
+                    pixels[counter] = bi.getRGB(i, j);
+                    counter++;
                 }
             }
 
-            pixel = (0xff000000 | red << 16 | green << 8 | blue);
-            pixels[i] = pixel;
-
-        }
-
-        counter = 0;
-        BufferedImage img = new BufferedImage(bi.getWidth(), bi.getHeight(), bi.getType());
-        for (int i = 0; i < bi.getWidth(); i++) {
-            for (int j = 0; j < bi.getHeight(); j++) {
-                img.setRGB(i, j, pixels[counter]);
-                counter++;
+            int[] levels = new int[level + 1];
+            int reste = 255 % level;
+            for (int i = 0; i < level; i++) {
+                levels[i] = (i + 1) * ((255 - reste) / level);
             }
-        }
+            levels[level] = 255;
 
-        System.out.println("J'ai fini");
-        g.drawImage(img, 0, 0, null);
-=======
-    public void posterize(Graphics g, String path) throws IOException {
-        short[] posterize = new short[256];
-        for (int i = 0; i < 256; i++) {
-            posterize[i] = (short) (i - (i % 32));
-        }
-        BufferedImageOp posterizeOp
-                = new LookupOp(new ShortLookupTable(0, posterize), null);
-        BufferedImage bi = ImageIO.read(new File(path));
-        
-        BufferedImage result = new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_INT_RGB);
-        result = posterizeOp.filter(bi, null);
+            for (int i = 0; i < pixels.length; i++) {
+                int pixel = pixels[i];
+                int red = (pixel & 0x00ff0000) >> 16;
+                int green = (pixel & 0x0000ff00) >> 8;
+                int blue = (pixel & 0x000000ff);
 
-        g.drawImage(result, 0, 0, null);
->>>>>>> master
+                boolean redChanged = false;
+                boolean greenChanged = false;
+                boolean blueChanged = false;
+                for (int j = 0; j < level; j++) {
+                    if (red < levels[j] && !redChanged) {
+                        red = (int) Math.floor(levels[j] / 2);
+                        redChanged = true;
+                    }
+                    if (green < levels[j] && !greenChanged) {
+                        green = (int) Math.floor(levels[j] / 2);
+                        greenChanged = true;
+                    }
+                    if (blue < levels[j] && !blueChanged) {
+                        blue = (int) Math.floor(levels[j] / 2);
+                        blueChanged = true;
+                    }
+
+                    if (redChanged && greenChanged && blueChanged) {
+                        break;
+                    }
+                }
+
+                pixel = (0xff000000 | red << 16 | green << 8 | blue);
+                pixels[i] = pixel;
+
+            }
+
+            counter = 0;
+            BufferedImage img = new BufferedImage(bi.getWidth(), bi.getHeight(), bi.getType());
+            for (int i = 0; i < bi.getWidth(); i++) {
+                for (int j = 0; j < bi.getHeight(); j++) {
+                    img.setRGB(i, j, pixels[counter]);
+                    counter++;
+                }
+            }
+
+            return new ImageIcon(img);
+        }
+        return new ImageIcon(ImageIO.read(new File(path)));
     }
 
-    //TODO regarder PDF pour fonctions restantes
 }
